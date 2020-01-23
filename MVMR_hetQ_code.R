d5b04eda7915cf536380ffb874392522855f7f05
eleanorsanderson <51537806+eleanorsanderson@users.noreply.github.com>
2020-01-23 13:09:59 +0000

    Added calculation with covariance

--- MVMR_hetQ_code.R
+++ MVMR_hetQ_code.R
@@ -21,3 +21,17 @@ Q <- sum(Q_snp)
 critical_value <- qchisq(0.05,L-2,lower.tail = FALSE)
 p_value <- pchisq(Q,L-2,lower.tail = FALSE)
 
+
+#If the SNP-exposure associations for each exposure come from the same data and
+#the (phenotypic) correlation between the exposures is avaliable the covariance between the errors can be approximated as;
+
+cov12 <- corr12*mvmrdata$x1.se*mvmrdata$x2.se
+
+# corr12 <- phenotypic correlation between x1 and x2
+
+# The Q statistic should then be estimated as; 
+seQ <- (mvmrdata$out.se)^2 + (estbeta1^2)*(mvmrdata$x1.se^2) + (estbeta2^2)*(mvmrdata$x2.se^2) + 2*(estbeta1*estbeta2)*(cov12)
+Q_snp <- ((1/seQ)*((mvmrdata$out.beta-(estbeta1*mvmrdata$x1.beta+estbeta2*mvmrdata$x2.beta))^2))
+Q <- sum(Q_snp)
+critical_value <- qchisq(0.05,L-2,lower.tail = FALSE)
+p_value <- pchisq(Q,L-2,lower.tail = FALSE)
